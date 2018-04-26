de726e91eccd74b2a8b0c7860a227d8e77566c86
Jonas Wloka <jowlo@uni-bremen.de>
2018-04-26 21:27:26 +0200

    Adapt to common API

--- crypto_kem/ntruhrss701/ref/api.h
+++ crypto_kem/ntruhrss701/ref/api.h
@@ -3,17 +3,13 @@
 
 #include "params.h"
 
+#include "pq_api.h"
+
 #define CRYPTO_ALGNAME "NTRU-KEM-HRSS17"
 
 #define CRYPTO_SECRETKEYBYTES NTRU_SECRETKEYBYTES
 #define CRYPTO_PUBLICKEYBYTES NTRU_PUBLICKEYBYTES
 #define CRYPTO_CIPHERTEXTBYTES NTRU_CIPHERTEXTBYTES
 #define CRYPTO_BYTES NTRU_SHAREDKEYBYTES
-
-int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
-
-int crypto_kem_enc(unsigned char *c, unsigned char *k, const unsigned char *pk);
-
-int crypto_kem_dec(unsigned char *k, const unsigned char *c, const unsigned char *sk);
 
 #endif
--- crypto_kem/saber/ref/api.h
+++ crypto_kem/saber/ref/api.h
@@ -7,9 +7,12 @@
 
 
 //   This is a sample 'api.h' for use 'sign.c'
+//
 
-#ifndef api_h
-#define api_h
+#ifndef SABER_api_h
+#define SABER_api_h
+
+#include "pq_api.h"
 
 #define CRYPTO_ALGNAME "Saber"
 
@@ -17,9 +20,5 @@
 #define CRYPTO_PUBLICKEYBYTES (3*320+32)
 #define CRYPTO_BYTES 32
 #define CRYPTO_CIPHERTEXTBYTES 1088
-
-int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
-int crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
-int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned char *sk);
 
 #endif /* api_h */
