ebabfe2fdbadabb1201afbd4f2da80665d611ce8
Jonas Wloka <jowlo@uni-bremen.de>
2018-04-26 22:19:10 +0200

    Move defines

--- crypto_kem/frodo640-cshake/ref/api.h
+++ crypto_kem/frodo640-cshake/ref/api.h
@@ -9,6 +9,20 @@
 
 #include "pq_api.h"
 
+// Parameters for "FrodoKEM-640"
+#define PARAMS_N 640
+#define PARAMS_NBAR 8
+#define PARAMS_LOGQ 15
+#define PARAMS_Q (1 << PARAMS_LOGQ)
+#define PARAMS_EXTRACTED_BITS 2
+#define PARAMS_STRIPE_STEP 8
+#define PARAMS_PARALLEL 4
+#define BYTES_SEED_A 16
+#define BYTES_MU (PARAMS_EXTRACTED_BITS*PARAMS_NBAR*PARAMS_NBAR)/8
+
+// Selecting cSHAKE XOF function for the KEM and noise sampling
+#define cshake    cshake128_simple
+
 #define CRYPTO_SECRETKEYBYTES  19872     // sizeof(s) + CRYPTO_PUBLICKEYBYTES + 2*PARAMS_N*PARAMS_NBAR
 #define CRYPTO_PUBLICKEYBYTES   9616     // sizeof(seed_A) + (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8
 #define CRYPTO_BYTES              16
