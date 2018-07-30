7d5637bff3ace90622ec6c113121c7886288470a
Jonas Wloka <jowlo@uni-bremen.de>
2018-07-30 11:41:24 +0200

    Add m4 fips202 from original pqm4

--- common/m4/fips202.h
+++ common/m4/fips202.h
@@ -1,18 +1,30 @@
 #ifndef FIPS202_H
 #define FIPS202_H
 
+#include <stdint.h>
+
 #define SHAKE128_RATE 168
+#define SHAKE256_RATE 136
 #define SHA3_256_RATE 136
+#define SHA3_512_RATE  72
 
 void shake128_absorb(uint64_t *s, const unsigned char *input, unsigned int inputByteLen);
 void shake128_squeezeblocks(unsigned char *output, unsigned long long nblocks, uint64_t *s);
-void shake128(unsigned char *output, unsigned int outputByteLen, const unsigned char *input, unsigned int inputByteLen);
-void sha3_256(unsigned char *output, const unsigned char *input, unsigned int inputByteLen);
+void shake128(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
 
+void cshake128_simple_absorb(uint64_t *s, uint16_t cstm, const unsigned char *in, unsigned long long inlen);
+void cshake128_simple_squeezeblocks(unsigned char *output, unsigned long long nblocks, uint64_t *s);
+void cshake128_simple(unsigned char *output, unsigned long long outlen, uint16_t cstm, const unsigned char *in, unsigned long long inlen);
 
-void KeccakF1600_StatePermute(uint64_t *);
-void KeccakF1600_StateXORBytes(void *state, const unsigned char *data, unsigned int offset, unsigned int length);
-void KeccakF1600_StateExtractBytes(void *state, const unsigned char *data, unsigned int offset, unsigned int length);
+void shake256_absorb(uint64_t *s, const unsigned char *input, unsigned int inputByteLen);
+void shake256_squeezeblocks(unsigned char *output, unsigned long long nblocks, uint64_t *s);
+void shake256(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
 
+void cshake256_simple_absorb(uint64_t *s, uint16_t cstm, const unsigned char *in, unsigned long long inlen);
+void cshake256_simple_squeezeblocks(unsigned char *output, unsigned long long nblocks, uint64_t *s);
+void cshake256_simple(unsigned char *output, unsigned long long outlen, uint16_t cstm, const unsigned char *in, unsigned long long inlen);
+
+void sha3_256(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
+void sha3_512(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
 
 #endif
